e15a975f21cdecbd0323e6236b5f340f6f91c535
Luiz Fernando <luizfrs2013@gmail.com>
2018-09-23 00:19:21 -0300

    First program

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+//First Program
+
+using namespace std;
+
+int main()
+{
+	int numberOne, numberTwo, sum;
+
+	cin>>numberOne>>numberTwo, sum;
+
+	sum = numberOne + numberTwo;
+
+	cout<<"X = "<<sum<<endl;
+
+	return 0;
+}
